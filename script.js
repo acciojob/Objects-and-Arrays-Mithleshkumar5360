eac81d71c83a0cda6268e2dc33148d21d3a5c915
Mithleshkumar5360 <117704988+Mithleshkumar5360@users.noreply.github.com>
2023-03-20 14:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team = players;
+const team1 =['john','Bob','Alice','Poppy'];
+const cap1 = {
+	name: 'john Doe',
+	age : 80
+};
 
 
 export {players,person,team,team1,cap1}
